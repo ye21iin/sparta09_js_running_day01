723f44338b44188f4bcbd3491c3567d049139f76
ye21iin <t2023-m0025@t2023-m0025ui-MacBookAir.local>
2025-01-06 16:43:23 +0900

    quiz02 answer

---
+++ quiz02.js
@@ -0,0 +1,22 @@
+// ## 2. 객체 선언해보기
+
+// 지난 시간에 배웠던 객체(object)를 다시 복습해보며, 아래의 조건을 충족하는 객체를 직접 선언해보세요.
+
+// - 자기 자신을 소개하는 객체입니다.
+// - 이름, 나이, MBTI 세 가지 키와 값이 포함되어 있어야 합니다.
+// - 콘솔 창에 이름, 나이, MBTI가 나오도록 console.log() 를 찍어보세요.
+
+// Answer
+
+```javascript
+const ye21iin = {
+  // 조건을 충족하는 코드 작성
+  name: "yelin",
+  age: 24,
+  mbti: "ISTJ",
+};
+
+console.log(ye21iin.name);
+console.log(ye21iin.age);
+console.log(ye21iin.mbti);
+```;
